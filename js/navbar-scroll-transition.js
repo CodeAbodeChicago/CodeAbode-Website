c91cf3cf16d4b1b9bfafe23049fc746230857c52
Michael Hadley <michaelwesthadley@gmail.com>
2016-01-20 16:48:20 -0600

    Update 2016-01-20T22:48:17.815Z

--- js/navbar-scroll-transition.js
+++ js/navbar-scroll-transition.js
@@ -1,2 +1,2 @@
-$(document).ready(function(){var a=$(".banner-large, .banner-small"),n=$(".navbar");if(!a.length)return void n.addClass("navbar-past-banner");n.addClass("navbar-transition");var r=a.offset().top+a.outerHeight(!1),e=r-n.outerHeight(!1);$(document).scroll(function(){var a=$(this).scrollTop();a>=e?n.addClass("navbar-past-banner"):n.removeClass("navbar-past-banner")})});
+$(document).ready(function(){var a=$(".banner-title"),n=$(".navbar");if(!a.length)return void n.addClass("navbar-past-banner");n.addClass("navbar-transition");var r=a.offset().top,t=(r-n.outerHeight(!1))/2;$(document).scroll(function(){var a=$(this).scrollTop();a>=t?n.addClass("navbar-past-banner"):n.removeClass("navbar-past-banner")})});
 //# sourceMappingURL=maps/navbar-scroll-transition.js.map
