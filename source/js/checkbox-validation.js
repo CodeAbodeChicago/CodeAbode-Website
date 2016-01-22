e966d0f77ea5351b2bf96377ffaf6aab9dd30345
retwedt <retwedt@gmail.com>
2016-01-22 13:55:13 -0600

    Custom form validation for checkboxes

---
+++ source/js/checkbox-validation.js
@@ -0,0 +1,44 @@
+// validate checkboxes in Application form
+
+// get elements from dom
+var form = document.querySelector("#apply-form");
+var checkboxes = form.elements["course"];
+var submit = form.elements["submit"];
+var checkboxErrMsg = document.querySelector("#check");
+var checked = false; // true if any course has been selected
+
+// check validation on change or submit
+for (var i = 0; i < checkboxes.length; i++) {
+	checkboxes[i].onchange = validateCheckboxes;
+}
+form.onchange = disableBtn;
+form.onsubmit = validateCheckboxes;
+
+// function to check if checkboxes have been validly filled out
+function validateCheckboxes(event) {
+	checked = false;
+	checkboxes = form.elements["course"];
+	for (var i = 0; i < checkboxes.length; i++) {
+		if (checkboxes[i].checked) {
+			checked = true;
+		}
+	}
+
+	if (!checked) {
+		event.preventDefault();
+		checkboxErrMsg.textContent = "Please select at least one course.";
+		submit.classList.add("poop");
+	} else {
+		checkboxErrMsg.textContent = "";
+		submit.classList.remove("popp");
+	}
+
+}
+
+function disableBtn() {
+	if (!checked) {
+		submit.classList.add("poop");
+	} else {
+		submit.classList.remove("poop");
+	}
+}
